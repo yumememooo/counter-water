aea85efa5f2e4a1f073cd7c89f96a458b00ab24c
yumememoooo <yumememooo@gmail.com>
2021-04-04 12:12:35 +0800

    Add: :seedling: rafc - ReactArrowFunctionComponent

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import reset from 'react-style-reset';
 import { injectGlobal } from '@emotion/css';
 injectGlobal(reset, {
 });
+
+//const Hello: () => JSX.Element
+//rafc - ReactArrowFunctionComponent
+//const Hello = () => {
+//   return (
+//     <div>
+//       hello
+//     </div>
+//   )
+// }
+//如果不寫retrun則{}要改成（）
+
+
+const Hello = () =>
+(
+  <div>
+    hello
+  </div>
+);
+
+
 export default function App() {
   return (
     <div className="App">
-      My Counter 
-      <Counter />
+      My Counter
+      <Hello />
     </div>
   );
 }
